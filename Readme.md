366842948b8e722681a791cc5108f951a61161e3
Daniel Goude <daniel@goude.se>
2017-04-01 20:37:03 +0200

    ud

--- Readme.md
+++ Readme.md
@@ -1,5 +1,8 @@
 # thxtime
 
+Render a "time string", for use in tmux and whatnot.
+
 Because when it's 11:38 (or 3:14), you can save a few brain cycles by
 letting your computer perform the association for you.
 
+Also, scope creep is fun.
--- thxtime/thxtime.py
+++ thxtime/thxtime.py
@@ -4,25 +4,36 @@ import os
 import time
 from datetime import datetime
 
-def get_time_string(t):
+
+def time_string(t):
     thxtime_path = os.environ['THXTIME_PATH']
     datafile = os.path.join(thxtime_path, 'data/thxdata.json')
 
     with open(datafile, 'rb') as data_fh:
         data = json.load(data_fh)
-        current_time_24 = t.strftime("%H:%M")
-        current_time_12 = t.strftime("%I:%M")
-        if current_time_24 in data:
-            return data[current_time_24]
-        elif current_time_12 in data:
-            return data[current_time_12]
-        else:
-            return current_time_24
+        return render_time(t, data)
+
+
+def render_time(t, data):
+    current_time_24 = t.strftime("%H:%M")
+    current_time_12 = t.strftime("%I:%M")
+
+    if current_time_24 in data:
+        return data[current_time_24]
+    elif current_time_12 in data:
+        return data[current_time_12]
+    else:
+        return current_time_24
+
+
+def thx_time(t):
+    return time_string(t)
+
 
 def main():
     now = datetime.fromtimestamp(time.time()).time()
-    print(get_time_string(now))
+    print(thx_time(now))
+
 
 if __name__ == '__main__':
     main()
-
