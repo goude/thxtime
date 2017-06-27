3d86c57b76df8d44f77c6a2ea3b57451f29e938a
Daniel Goude <daniel@goude.se>
2017-06-27 19:09:52 +0200

    refactor

--- thxtime/thxtime.py
+++ thxtime/thxtime.py
@@ -1,36 +1,7 @@
 from __future__ import print_function
 import time
-from datetime import datetime as dt
+import datetime
 
-
-def time_string(t):
-    data = {
-        "3:14": "π",
-        "6:28": "τ",
-        "11:06": "Hastings",  # 10:66 = 10:60 + 6 = 11:06
-        "11:38": "THX",
-        "13:37": "Teel",
-        "15:17": "95LUTH",
-        "22:27": "Baker St."  # 1B = 27
-    }
-
-    return render_time(t, data)
-
-
-def render_time(t, data):
-    current_time_24 = t.strftime("%H:%M")
-    current_time_12 = t.strftime("%I:%M")
-
-    if current_time_24 in data:
-        return data[current_time_24]
-    elif current_time_12 in data:
-        return data[current_time_12]
-    else:
-        return current_time_24
-
-
-def thx_time(t):
-    return time_string(t)
 
 # https://stackoverflow.com/questions/1969240/mapping-a-range-of-values-to-another
 def translate(value, leftMin, leftMax, rightMin, rightMax):
@@ -44,7 +15,12 @@ def translate(value, leftMin, leftMax, rightMin, rightMax):
     # Convert the 0-1 range into a value in the right range.
     return rightMin + (valueScaled * rightSpan)
 
+
 def second_spinner(t):
+    '''
+    Given a datetime, return a symbol string representing the number
+    of seconds.
+    '''
     symbols = [
         '◷',
         '◶',
@@ -54,21 +30,50 @@ def second_spinner(t):
     index = int(translate(t.second, 0, 60, 0, 4))
     return symbols[index]
 
-def thx_time_extended(t):
+
+def thx_time(dt):
+    ''' Given a datetime, return a string representing the current time.'''
+    data = {
+        "3:14": "π",
+        "6:28": "τ",
+        "11:06": "Hastings",  # 10:66 = 10:60 + 6 = 11:06
+        "11:38": "THX",
+        "13:37": "Teel",
+        "15:17": "95LUTH",
+        "22:27": "Baker St."  # 1B = 27
+    }
+
+    current_time_24 = dt.strftime("%H:%M")
+    current_time_12 = dt.strftime("%I:%M")
+
+    if current_time_24 in data:
+        return data[current_time_24]
+    elif current_time_12 in data:
+        return data[current_time_12]
+    else:
+        return current_time_24
+
+
+def thx_time_extended(dt):
+    ''' Given a datetime, return a string representing several time measures.
+    '''
     time_str = 'W{} {} U{}·{} {}'.format(
         dt.now().isocalendar()[1],
         dt.now().strftime('%m-%d'),
         dt.utcnow().strftime('%H'),
-        time_string(t),
-        second_spinner(t)
+        thx_time(dt),
+        second_spinner(dt)
     )
     return time_str
 
 
-def main():
-    now = dt.fromtimestamp(time.time()).time()
-    print(thx_time_extended(now))
+def thx_time_now():
+    return thx_time(datetime.datetime.now())
+
+
+def thx_time_extended_now():
+    return thx_time_extended(datetime.datetime.now())
 
 
 if __name__ == '__main__':
-    main()
+    print(thx_time_extended_now())
